0aa6575c90c6a86d6093f30a0a2f56c932a96bd2
Sushant Sahu <sushant87@gmail.com>
2023-04-30 11:32:21 +0530

    feat: update

--- basics/define-ability.ts
+++ basics/define-ability.ts
@@ -1,12 +1,20 @@
 import { defineAbility } from "@casl/ability";
 
-export default (user: { id: number, isLoggedIn: boolean }) =>
+export default (user: {
+  id: number;
+  isLoggedIn: boolean;
+  isModerator: boolean;
+}) =>
   defineAbility((can) => {
-    can("read", "article");
+    can("read", "Article");
+    can("update", "Article", ["title", "description"], { authorId: user.id });
 
     if (user.isLoggedIn) {
       can("update", "Article", { authorId: user.id });
-    //   can("create", "Comment");
-    //   can("update", "Comment", { authorId: user.id });
+      can("create", "Comment");
+      can("update", "Comment", { authorId: user.id });
+    }
+    if (user.isModerator) {
+      can("update", "Article", ["published"]);
     }
   });
--- basics/index.ts
+++ basics/index.ts
@@ -1,16 +1,27 @@
 import defineAbilityFor from "./define-ability";
-import { Article } from './entities';
+import { Article } from "./entities";
 
-const user = { id: 1, isLoggedIn: true };
+const user = { id: 1, isLoggedIn: true, isModerator: false };
 const ability = defineAbilityFor(user);
 
-const canRead = ability.can("read", "article");
+const canRead = ability.can("read", "Article");
 console.log(canRead);
 
 const ownArticle = new Article({ authorId: user.id });
-const update = ability.can('update', ownArticle); 
+const update = ability.can("update", ownArticle);
 console.log(update);
 
-const someoneelseArticle = new Article({ authorId: 2 });
-const can_update = ability.can('update', someoneelseArticle);
+const foreignArticle = new Article({ authorId: 2 });
+const can_update = ability.can("update", foreignArticle);
 console.log(can_update);
+
+const can_create = ability.can("create", "Comment");
+console.log(can_create);
+
+const moderator = { id: 3, isModerator: true, isLoggedIn: true };
+
+const ability_1 = defineAbilityFor(moderator);
+const update_title = ability_1.can("update", "Article", "published");
+console.log(update_title);
+console.log(ability_1.can('update', ownArticle, 'published'));
+console.log(ability_1.can('update', foreignArticle, 'title'))
